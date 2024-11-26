bbee3d9360d04d220cd0fc0954389f5c83910fc4
vdamjanoski <vladimirdamjanoski@yahoo.com>
2024-11-26 15:17:24 +0100

    .

--- Domashno 4/script.js
+++ Domashno 4/script.js
@@ -1,26 +1,27 @@
 async function fetchPhones() {
-    var response = await fetch("https://api.restful-api.dev/objects");
-    var data = await response.json();
-    return data;
-  }
-  var Phones;
-  console.log(Phones);
-  fetchPhones().then((Phones) => {
-    console.log(Phones)
-    Phones.forEach(element => {
-        document.getElementById("search").innerHTML+=`
-        <div class="element">
-        <p>${element.id}</p>
-        <p>${element.name}</p>
-        <p>${element.data}</p>
-        <p>${element.data.color}</p>
+  var response = await fetch("https://api.restful-api.dev/objects");
+  var data = await response.json();
+  return data;
+}
+var Phones;
+console.log(Phones);
+fetchPhones().then((Phones) => {
+  console.log(Phones)
+  Phones.forEach(element => {
+      document.getElementById("search").innerHTML+=`
+      <div class="element">
+      <p>${element.id}</p>
+      <p>${element.name}</p>
+      <p>${element.data}</p>
 
-        </div>`
-    });
-  })
+      </div>`
+  });
+})
 var searchBtn = document.getElementById("search-button");
 var searchInput = document.getElementById("search-input");
 
+searchBtn.addEventListener("click", () => {
+  console.log("clicked");
+});
 
-  
-  
+
