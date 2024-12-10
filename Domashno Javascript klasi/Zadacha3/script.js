90a2e7b8d51d5dabfa59726053166131fab9f1f0
vdamjanoski <vladimirdamjanoski@yahoo.com>
2024-12-10 17:40:10 +0100

    2323

--- Domashno Javascript klasi/Zadacha3/script.js
+++ Domashno Javascript klasi/Zadacha3/script.js
@@ -12,13 +12,8 @@ class BankAccount {
             <label for="button1"><button id="depositBtn" onclick="deposit()">Deposit</button></label>
             <label for="button2"><button id="withdrawBtn" onclick="withdraw()">Withdraw</button></label>
             <label for="transactions"><button id="transactionBtn" onclick="transactions()">Transactions</button></label>
-            <div class="balanceDiv" style="display: none;">Your balance is: $${this.balance}</div>
             </div>`;
     }
-    displayBalance() {
-      console.log(`Your balance is: $${this.balance}`);
-    }
-    
   } 
   
   const myBankAccount = new BankAccount("12371674", parseInt(500));
@@ -53,6 +48,4 @@ function withdraw(){
         showDiv.innerHTML+=`<div>Your new balance is $${(myBankAccount.balance)}!</div>`;
     }
 }
-function transactions(){
-
-}
+const transactionsDiv=document.getElementById("transactionsDiv");
