b258e947f84170b793265c531d0ab9107359e7ee
vdamjanoski <vladimirdamjanoski@yahoo.com>
2024-12-10 18:12:04 +0100

    End

--- Domashno Javascript klasi/Zadacha3/script.js
+++ Domashno Javascript klasi/Zadacha3/script.js
@@ -10,7 +10,7 @@ class BankAccount {
             <label for="search"><input type="number" id="searchInput"></label>
             <label for="button1"><button id="depositBtn" onclick="deposit()">Deposit</button></label>
             <label for="button2"><button id="withdrawBtn" onclick="withdraw()">Withdraw</button></label>
-            <label for="transactions"><button id="transactionBtn" onclick="transactions()">Transactions</button></label>
+            <label for="transactions"><button id="transactionBtn">Transactions</button></label>
             </div>`;
   }
 }
@@ -20,7 +20,7 @@ const myBankAccount = new BankAccount("12371674", parseInt(500));
 const depositBtn = document.getElementById("depositBtn");
 function deposit() {
   myBankAccount.balance += parseInt(searchInput.value);
-  showDiv.innerHTML += `<div><span>You deposited $${searchInput.value} .</span> Your new balance is $${myBankAccount.balance}!</div>`;
+  showDiv.innerHTML += `<div><span>You deposited $${searchInput.value}</span> Your new balance is $${myBankAccount.balance}!</div>`;
 }
 
 const withdrawBtn = document.getElementById("withdrawBtn");
