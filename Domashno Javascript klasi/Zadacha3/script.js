c8c422954e3bc90c46c3585b15eac16b03419673
vdamjanoski <vladimirdamjanoski@yahoo.com>
2024-12-10 17:43:38 +0100

    323232

--- Domashno Javascript klasi/Zadacha3/script.js
+++ Domashno Javascript klasi/Zadacha3/script.js
@@ -1,11 +1,10 @@
 class BankAccount {
-    constructor(accountNumber, balance) {
-      this.accountNumber = accountNumber;
-      this.balance = balance;
-        const showDiv=document.getElementById("showDiv");
+  constructor(accountNumber, balance) {
+    this.accountNumber = accountNumber;
+    this.balance = balance;
+    const showDiv = document.getElementById("showDiv");
 
-  
-      showDiv.innerHTML+=`<div class="bankCards">
+    showDiv.innerHTML += `<div class="bankCards">
             <div>Account number: ${accountNumber}</div>
             <div>Starting balance: $${balance}</div>
             <label for="search"><input type="number" id="searchInput"></label>
@@ -13,39 +12,24 @@ class BankAccount {
             <label for="button2"><button id="withdrawBtn" onclick="withdraw()">Withdraw</button></label>
             <label for="transactions"><button id="transactionBtn" onclick="transactions()">Transactions</button></label>
             </div>`;
-    }
-  } 
-  
-  const myBankAccount = new BankAccount("12371674", parseInt(500));
-  
-  const depositBtn = document.getElementById("depositBtn");
+  }
+}
 
-// //   depositBtn.addEventListener("click", () => {
-// //    myBankAccount.balance+=parseInt(searchInput.value);
-// //    showDiv.innerHTML+=`<div>Your new balance is $${(myBankAccount.balance)}!</div>`;
-// })
-function deposit(){
-    myBankAccount.balance+=parseInt(searchInput.value);
-   showDiv.innerHTML+=`<div>Your new balance is $${(myBankAccount.balance)}!</div>`;
+const myBankAccount = new BankAccount("12371674", parseInt(500));
+
+const depositBtn = document.getElementById("depositBtn");
+function deposit() {
+  myBankAccount.balance += parseInt(searchInput.value);
+  showDiv.innerHTML += `<div><span>You deposited $${searchInput.value} .</span> Your new balance is $${myBankAccount.balance}!</div>`;
 }
 
 const withdrawBtn = document.getElementById("withdrawBtn");
-
-//   withdrawBtn.addEventListener("click", () => {
-
-//     if (myBankAccount.balance<searchInput.value){
-//         showDiv.innerHTML+=`<div>Insufficient balance!</div>`;
-//     }else{
-//         myBankAccount.balance-=searchInput.value;
-//         showDiv.innerHTML+=`<div>Your new balance is $${(myBankAccount.balance)}!</div>`;
-//     }
-// })
-function withdraw(){
-    if (myBankAccount.balance<searchInput.value){
-        showDiv.innerHTML+=`<div>Insufficient balance!</div>`;
-    }else{
-        myBankAccount.balance-=searchInput.value;
-        showDiv.innerHTML+=`<div>Your new balance is $${(myBankAccount.balance)}!</div>`;
-    }
+function withdraw() {
+  if (myBankAccount.balance < searchInput.value) {
+    showDiv.innerHTML += `<div>Insufficient balance!</div>`;
+  } else {
+    myBankAccount.balance -= searchInput.value;
+    showDiv.innerHTML += `<div><span>You withdrawed $${searchInput.value} .</span> Your new balance is $${myBankAccount.balance}!</div>`;
+  }
 }
-const transactionsDiv=document.getElementById("transactionsDiv");
+const transactionsDiv = document.getElementById("transactionsDiv");
